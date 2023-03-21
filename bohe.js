c350b5c1214f09b0b38513ac2c5da8300354141b
He <52130473+guodahe@users.noreply.github.com>
2023-03-21 23:26:04 +0800

    Add files via upload

---
+++ bohe.js
@@ -0,0 +1,12 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.vip.state="1"   
+obj.vip.integral_member.integral=9999
+obj.vip.integral_member.level=9999
+obj.vip.is_member=true
+obj.vip.started_at="2023-03-20"
+obj.vip.started_at="2099-12-12"
+obj.user.role=1
+obj.user.user_type=1
+body = JSON.stringify(obj);
+$done(body);
