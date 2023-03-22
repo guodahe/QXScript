56df0906049841bfcee6ac72c2727a141aeb1b67
He <52130473+guodahe@users.noreply.github.com>
2023-03-22 11:59:20 +0800

    Update bohe.js

--- bohe.js
+++ bohe.js
@@ -1,13 +1,12 @@
 var obj = JSON.parse($response.body);
 console.log(obj);
 //obj.vip.state="1"   
-//obj.vip.integral_member.integral=9999
-//obj.vip.integral_member.level=9999
-//obj.vip.is_member=true
-obj.data.vip.started_at="2023-03-20"
-obj.data.vip.started_at="2099-12-12"
+obj.vip.integral_member.integral=9999;
+obj.data.vip.integral_member.level=9999;
+obj.data.vip.started_at="2023-03-20";
+obj.data.vip.started_at="2099-12-12";
 //obj.user.role=1
-obj.data.user.user_type=2
+obj.data.user.user_type=2;
 obj.data.vip.is_member=true;
 obj.data.user.user_name="测试Qqq";
 $done({body:JSON.stringify(obj)});
