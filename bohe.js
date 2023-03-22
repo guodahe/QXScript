1891ee32a2d950651a2811917722e84971b551ed
He <52130473+guodahe@users.noreply.github.com>
2023-03-22 14:54:58 +0800

    Update bohe.js

--- bohe.js
+++ bohe.js
@@ -5,6 +5,10 @@ var obj = JSON.parse($response.body);
 if(/app-interface\/v1\/user\/index/.test(url)){
     obj.data.first =  obj.data.first.map(v=>{
         v.is_gray=false
+        v.track= {
+            "type": "",
+            "track_points": []
+        }
         return v
     });
 }else if(/api\/v1\/user\_profile\/detail/.test(url)){
@@ -20,6 +24,24 @@ if(/app-interface\/v1\/user\/index/.test(url)){
     obj.data.user.user_type=9;
     obj.data.vip.is_member=true;
     obj.data.user.user_name="大河破解";
+}else if(/shop-interface\/v1\/member\/get\_vip\_subscribe\_info/.test(url)){
+    obj.data={
+        "state": "1",
+        "vip_subscribe_info": {
+            "id": 0,
+            "vip_subscribe_no": "123456",
+            "user_key": "123456",
+            "user_name": "大河破解",
+            "state": "1",
+            "signing_time": "2023-03-20",
+            "canceled_time": "2099-12-12",
+            "renewal_count": 0,
+            "source": "app",
+            "charge_interval": "",
+            "sku_id": 0
+        },
+        "show_type": 1
+    }
 }
 
 $done({body:JSON.stringify(obj)});
