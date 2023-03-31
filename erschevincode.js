c17631f13066a067ec3b62509db540c31b117510
He <52130473+guodahe@users.noreply.github.com>
2023-03-31 15:10:52 +0800

    Update erschevincode.js

--- erschevincode.js
+++ erschevincode.js
@@ -12,10 +12,10 @@ const isJSBox = typeof $app != "undefined" && typeof $http != "undefined";
 // 判断是否是Node环境
 const isNode = typeof require == "function" && !isJSBox;
 
-let linkurl = decodeURI(obj.result.vb.linkurl)
-let index= linkurl.indexOf('vincode=')+8
-let vincode =linkurl.substr(index,17)
-notify("二手车之家","获取车架号vincode","vincode:"+vincode)
+let linkurl = decodeURI(obj.result.vb.linkurl);
+let index= linkurl.indexOf('vincode=')+8;
+let vincode =linkurl.substr(index,17);
+notify("二手车之家","获取车架号vincode","vincode:"+vincode);
 
 /**
  * 提示信息
