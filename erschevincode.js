60fce5b25fea9b614e78299b7ff2cf9de6aabfcf
He <52130473+guodahe@users.noreply.github.com>
2023-03-31 15:24:42 +0800

    Update erschevincode.js

--- erschevincode.js
+++ erschevincode.js
@@ -2,6 +2,8 @@ var obj = JSON.parse($response.body);
 
 
 let linkurl = decodeURI(obj.result.vb.linkurl);
+console.log(linkurl);
 let index= linkurl.indexOf('vincode=')+8;
+console.log(index);
 let vincode =linkurl.substr(index,17);
 $notify("二手车之家","获取车架号vincode","vincode:"+vincode);
