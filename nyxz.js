74e4f137d821c052a5236f08e5f55ebc992914e0
He <52130473+guodahe@users.noreply.github.com>
2023-04-12 17:54:55 +0800

    Update nyxz.js

--- nyxz.js
+++ nyxz.js
@@ -1,3 +1,3 @@
-var requestBody = $request
-requestBody.body=requestBody.body.replace('00008110-000A40611A61401E','00008030-00096D9C3EC3802E');
-$notify('nyxz','请求体',requestBody.body);
+var requestBody = $request.body;
+requestBody=requestBody.replace('00008110-000A40611A61401E','00008030-00096D9C3EC3802E');
+$done();
