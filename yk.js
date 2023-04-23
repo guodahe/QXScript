7a16580c51aec14937f594d2b7ab4e6ac074356f
He <52130473+guodahe@users.noreply.github.com>
2023-04-23 15:04:15 +0800

    Update yk.js

--- yk.js
+++ yk.js
@@ -1,4 +1,5 @@
 var obj = JSON.parse($response.body);
 obj.admob_esp_processor_term_accepted= true;
+obj.auto_collect_location= false;
 console.log(JSON.stringify(obj));
 $done({body:JSON.stringify(obj)});
