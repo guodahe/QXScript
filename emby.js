78514f62d3479daa2fee95394e79a74eb361b8b4
He <52130473+guodahe@users.noreply.github.com>
2023-04-28 17:42:32 +0800

    Update emby.js

--- emby.js
+++ emby.js
@@ -2,6 +2,6 @@
  * http://www.emby.shop/newsystem/index.jsp 无需登录
  */
 var obj = JSON.parse($response.body);
-console.log(obj)
-$notify("emby", "获取账号密码", obj.extend.orderinfo.transNo)
+console.log(obj);
+$notify("emby", "获取账号密码", obj.extend.orderinfo.transNo);
 $done();
