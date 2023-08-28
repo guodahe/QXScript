c1a4cc414a438db95aefea0dc829cf4755aced75
He <52130473+guodahe@users.noreply.github.com>
2023-08-28 13:57:57 +0800

    Update qsq.js

--- qsq.js
+++ qsq.js
@@ -1,1 +1,9 @@
-$done({path: '/appstore?udid=c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a'});
+var url = $request.url;
+console.log($request);
+function replaceParamVal(oUrl, paramName, replaceWith) {
+    var re = new RegExp('(' + paramName + '=)([^&]*)', 'gi');
+    var nUrl = oUrl.replace(re, paramName + '=' + replaceWith);
+    return nUrl;
+};
+let newUrl = replaceParamVal(url, 'udid', 'c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
+$done({ url: newUrl });
