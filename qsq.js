1c0a9e60b99866f3e985856140767d11938468ab
He <52130473+guodahe@users.noreply.github.com>
2023-08-28 11:28:34 +0800

    Update qsq.js

--- qsq.js
+++ qsq.js
@@ -1,10 +1,10 @@
 var url = $request.url;
-console.info("替换前",url);
+console.log("替换前",url);
 function replaceParamVal(oUrl, paramName, replaceWith) {
     var re = new RegExp('(' + paramName + '=)([^&]*)', 'gi');
     var nUrl = oUrl.replace(re, paramName + '=' + replaceWith);
     return nUrl;
 };
 let newUrl = replaceParamVal(url, 'udid', 'c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
-console.info("替换后",newUrl);
+console.log("替换后",newUrl);
 $done({ url: newUrl });
