83040e6336eeb1ad3c810969a73e1f66fe604a40
He <52130473+guodahe@users.noreply.github.com>
2023-08-28 18:02:46 +0800

    Update qsq.js

--- qsq.js
+++ qsq.js
@@ -3,7 +3,10 @@
  * 
  */
 
- var url = $request.url;
-url = url.replace(/udid=(.*)/,'udid=c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
-console.log(url);
-$done({url});
+ var req = {};
+req.url = $request.url.replace(/udid=(.*)/,'udid=c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
+req.headers = $request.headers;
+req.method = $request.method;
+req.path = $request.path.replace(/udid=(.*)/,'udid=c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
+console.log(req);
+$done(req);
