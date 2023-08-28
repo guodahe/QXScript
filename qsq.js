9f98a91c524c13d665d92a5c1c4553c53dafd3d5
He <52130473+guodahe@users.noreply.github.com>
2023-08-28 18:24:54 +0800

    Update qsq.js

--- qsq.js
+++ qsq.js
@@ -6,6 +6,7 @@
  var req = {};
 req.url = $request.url.replace(/udid=(.*)/,'udid=c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
 req.headers = $request.headers;
+req.headers.X_CACHE_KEY="123";
 req.method = $request.method;
 req.path = $request.path.replace(/udid=(.*)/,'udid=c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
 console.log(JSON.stringify(req));
