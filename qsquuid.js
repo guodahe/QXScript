35c3f13eb5a42afab18769f1ccff8367f03d51a5
He <52130473+guodahe@users.noreply.github.com>
2023-08-28 15:04:51 +0800

    Update qsquuid.js

--- qsquuid.js
+++ qsquuid.js
@@ -1,5 +1,5 @@
 console.log("匹配到获取uuid路径了");
-$notify("uuid", "匹配到获取uuid路径了",'123')
+$notify("uuid", "匹配到获取uuid路径了",'123');
 var responseBody = $response.body;
 console.log(responseBody);
 let result = responseBody.replace("00008110-000A40611A61401E", "c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a");
