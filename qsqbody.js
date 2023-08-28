eeb684c1ac60878f16804ba8ec80a66e664c020b
He <52130473+guodahe@users.noreply.github.com>
2023-08-28 16:15:52 +0800

    Update qsqbody.js

--- qsqbody.js
+++ qsqbody.js
@@ -12,31 +12,18 @@
  let newUrl = replaceParamVal(url, 'udid', 'c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
 
  const method = `GET`;
- const headers = {
- 'Accept' : `*/*`,
- 'Accept-Encoding' : `gzip, deflate`,
- 'Connection' : `keep-alive`,
- 'Content-Type' : `application/json`,
- 'appstore' : `v2`,
- 'Host' : `api.fanhangame.ltd`,
- 'User-Agent' : `ESign/1 CFNetwork/1410.0.3 Darwin/22.6.0`,
- 'Agent' : `ESign`,
- 'version' : `4.8.2`,
- 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
- };
+
  const body = ``;
  
  const myRequest = {
      url: newUrl,
-     method: method,
-     headers: headers,
+     method: $request.method,
+     headers: $request.headers,
      body: body
  };
  
  $task.fetch(myRequest).then(response => {
-    console.log("请求成功");
      $done(response.body);
-   console.log("请求完成");
  }, reason => {
      console.log(reason.error);
      $done();
