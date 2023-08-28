57c39137e7fcc007858af62e20549ffcbeef49e9
He <52130473+guodahe@users.noreply.github.com>
2023-08-28 17:05:48 +0800

    Update qsq.js

--- qsq.js
+++ qsq.js
@@ -4,39 +4,5 @@
  */
 
  var url = $request.url;
- function replaceParamVal(oUrl, paramName, replaceWith) {
-     var re = new RegExp('(' + paramName + '=)([^&]*)', 'gi');
-     var nUrl = oUrl.replace(re, paramName + '=' + replaceWith);
-     return nUrl;
- };
- let newUrl = replaceParamVal(url, 'udid', 'c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
- const url =newUrl;
- const method = `GET`;
- const headers = {
- 'Accept' : `*/*`,
- 'Accept-Encoding' : `gzip, deflate`,
- 'Connection' : `keep-alive`,
- 'Content-Type' : `application/json`,
- 'appstore' : `v2`,
- 'Host' : `api.fanhangame.ltd`,
- 'User-Agent' : `ESign/1 CFNetwork/1410.0.3 Darwin/22.6.0`,
- 'Agent' : `ESign`,
- 'version' : `4.8.2`,
- 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
- };
- const body = ``;
- 
- const myRequest = {
-     url: url,
-     method: method,
-     headers: headers,
-     body: body
- };
- 
- $task.fetch(myRequest).then(response => {
-     console.log(response.statusCode + "\n\n" + response.body);
-     $done({body:response.body});
- }, reason => {
-     console.log(reason.error);
-     $done();
- });
+url = url.replace(/udid=(.*)/,'udid=c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a')
+$done({url})
