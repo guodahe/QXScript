10d116558414a5b4bb116224d7ba38b914102dc6
He <52130473+guodahe@users.noreply.github.com>
2023-08-29 18:15:27 +0800

    Update qsqbody.js

--- qsqbody.js
+++ qsqbody.js
@@ -9,7 +9,7 @@
       var nUrl = oUrl.replace(re, paramName + '=' + replaceWith);
       return nUrl;
   };
-  let newUrl = replaceParamVal(url, 'udid', 'c17440a56c5ef43b4e9381ffd60eb7cd7efdf61a');
+  let newUrl = replaceParamVal(url, 'udid', '00008110-000A40611A61401E');
 
   const method = `GET`;
 
