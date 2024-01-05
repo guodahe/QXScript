01418af1ce2e046e9807c764023e4fe745a07efd
He <52130473+guodahe@users.noreply.github.com>
2024-01-05 14:37:00 +0800

    Update bp.js

--- bp.js
+++ bp.js
@@ -2,5 +2,6 @@ var url = $request.url
 var obj = JSON.parse($response.body);
 obj.isManager = "1";
 obj.isDesigner = "1";
-
+obj.designerInfo[0].isOfficial=true;
+obj.designerInfo[0].memberFlag="1";
 $done({ body: JSON.stringify(obj) });
